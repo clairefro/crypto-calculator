e7666a28a1609e4b38e479598d1660f99dc0b2a8
claire froelich <claire.froelich@gmail.com>
2020-01-26 14:37:19 -0500

    add calculate_value and current_price methods to model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'currencies/index'
-  get 'currencies/search'
-  get 'currencies/calculate'
+  root 'currencies#index'
+  post 'search', to: 'currencies#search'
+  post 'calculate', to: 'currencies#calculate'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
