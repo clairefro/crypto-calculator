d2a383481e483358d04c9b0ff99ced2163026ff6
claire froelich <claire.froelich@gmail.com>
2020-01-26 13:09:49 -0500

    changed column name

--- db/schema.rb
+++ db/schema.rb
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2020_01_24_213520) do
   create_table "currencies", force: :cascade do |t|
     t.string "name"
     t.string "desc"
-    t.bigint "maxsupply"
+    t.bigint "max_supply"
     t.string "currency_symbol"
     t.string "slug"
     t.datetime "created_at", precision: 6, null: false
