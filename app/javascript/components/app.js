c4066a7ecf067d516a20fc76ce359b5979ac9762
claire froelich <claire.froelich@gmail.com>
2020-01-26 18:44:11 -0500

    implement post request search

--- app/javascript/components/app.js
+++ app/javascript/components/app.js
@@ -1,4 +1,11 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
+// configure axios with csrf token in all headers
+// get token from meta tag
+const csrfToken = document.querySelector('[name="csrf-token"]').content;
+// set axios common headers to csrf token
+axios.defaults.headers.common['X-CSRF-TOKEN']= csrfToken;
 
 import PortfolioContainer from './portfolio_container'
 
--- app/javascript/components/portfolio_container.js
+++ app/javascript/components/portfolio_container.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import Search from './search'
 import Calculate from './calculate'
 import Portfolio from './portfolio'
+
 
 class PortfolioContainer extends Component {
   constructor(props){
@@ -19,10 +21,16 @@ class PortfolioContainer extends Component {
 
   handleChange = (e) => {
     // e.target.name below is defined in the input field that calls this function on change
-    this.setState({
-      [e.target.name]: e.target.value
-    });
-    console.log(this.state.name)
+    // this.setState({
+    //   [e.target.name]: e.target.value
+    // });
+    console.log(e.target.value);
+
+    axios.post('http://localhost:3000/search', {
+      search: e.target.value
+    })
+    .then(data => {debugger})
+    .catch(data => {debugger});
   }
 
   render() {
