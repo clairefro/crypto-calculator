cedb301df18ff6b2cc10daae4512f9a8f74adb33
claire froelich <claire.froelich@gmail.com>
2020-01-26 14:43:23 -0500

    implement #calculate

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -3,8 +3,24 @@ class CurrenciesController < ApplicationController
   end
 
   def search
+    @currencies = Currency.where('LOWER(name) ISLIKE ?', "%#{params[:search].downcase}")
+    render json: { currencies: @currencies }
   end
 
+  # takes in currency id and amount owned, returns calculations
   def calculate
+    amount = params[:amount]
+    render json: {
+      currency: currency,
+      current_price: current_price,
+      amount: amount,
+      value: currency.calculate_value
+    }
+  end
+
+  private
+
+  def currency
+    @currency ||= Currency.find(params[:id])
   end
 end
